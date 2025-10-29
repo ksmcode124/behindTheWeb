1e24bd408888834db7e9b0f2e78028b0c59c24a5
Indra-Haha <124240180@student.upnyk.ac.id>
2025-10-29 09:21:14 +0700

    fix(Logic) : fixed coba carousel --kurang animasi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,44 +13,46 @@ const players = [
 
 export default function Home() {
   const [startIndex, setStartIndex] = useState<number>(0); // index awal window
-  const handleNext = () => {
-    setStartIndex((prev) => Math.min(prev + 1, players.length - 5)); // geser window ke kanan
+  const handlePrev = () => {
+     setStartIndex((prev) => (prev + 1) % players.length); // geser window ke kanan
     console.log("next")
   };
 
-  const handlePrev = () => {
-    setStartIndex((prev) => Math.max(prev - 1, 0)); // geser window ke kiri
+  const handleNext = () => {
+    setStartIndex((prev) => (prev - 1 + players.length) % players.length); // geser window ke kiri
     console.log("prev")
   };
 
+   // Fungsi untuk mengambil 5 nama (looping)
+  const getVisibleNames = () => {
+    return Array.from({ length: 5 }, (_, i) => {
+      const index = (startIndex + i) % players.length; // loop index
+      return players[index];
+    });
+  };
+  
   let count: number = 0;
-  const ButtonPosition = () => {
-    count++;
+  const ButtonPosition = (size: 1 | 2 | 3) => {
+    size === 2 ?  count++ : "";
     return (count % 2 === 0 ? handlePrev : count % 2 === 1 ? handleNext : "");
   };
   
   return (
-    <div className="font-sans flex flex-row items-center justify-items-center min-h-screen p-8 pb-20 gap-8 sm:p-20 bg-black">
-
-
-      {players.slice(startIndex, startIndex + 5).map((player, index) => {
-        const size: 1 | 2 | 3 = index < 3
-          ? (3 - index) as 1 | 2 | 3   // 0→3, 1→2, 2→1
-          : (index - 1) as 1 | 2 | 3;  // 3→2, 4→3
-        size === 2 ? ButtonPosition() : "";
-        return (ButtonPosition() ? <button
+    <div className="font-sans flex flex-row items-center justify-center  min-h-screen p-8 pb-20 gap-8 sm:p-20 bg-black">
+      {getVisibleNames().map((player, i)  => {
+        const size: 1 | 2 | 3 = i < 3
+          ? (3 - i) as 1 | 2 | 3   // 0→3, 1→2, 2→1
+          : (i - 1) as 1 | 2 | 3;  // 3→2, 4→3
+        const isButton = size === 2;
+        return isButton ? <button
           key={player}
-          onClick={ButtonPosition()}
+          onClick={ButtonPosition(size)}
           className="flex w-auto h-auto"
         >
           <FlipCard size={size} imageSrc={player} />
         </button> :
-
-           <FlipCard size={size} imageSrc={player} />
-          );
+           <FlipCard key={player} size={size} imageSrc={player} />
       })}
-
-
     </div>
   );
 }
