765be8a549540cb70e223663752b581cd1b6794c
dndraamy <dndramylza12@gmail.com>
2025-12-07 02:03:59 +0700

    add responsiveness

--- src/app/components/sections/Footer/Footer.tsx
+++ src/app/components/sections/Footer/Footer.tsx
@@ -26,24 +26,30 @@ const socialMediaData = [
   },
 ];
 
-export function Footer() {
+export default function Footer() {
   return (
-    <footer className="bg-secondary-300 relative h-[350px] text-white">
-      <div className="mx-auto flex max-w-7xl flex-col items-start justify-between pt-[65px] pr-8 pl-[140px] md:flex-row md:items-center">
-        <div className="flex flex-col space-y-6 md:w-1/2">
+    <footer className="relative min-h-[350px] bg-[#393C45] text-white">
+      <div className="mx-auto flex max-w-7xl flex-col items-start justify-start px-6 pt-10 pb-10 lg:flex-row lg:justify-between lg:pt-[65px] lg:pr-8 lg:pb-0 lg:pl-[140px]">
+        {/* Kolom Kiri: Logo dan Deskripsi */}
+        <div className="flex w-full flex-col space-y-4 lg:w-1/2 lg:space-y-6">
           <div className="flex items-center space-x-3">
-            <Logo variant="white" />
+            <div className="relative h-[30px] w-[35px] lg:h-[38px] lg:w-[43px]">
+              <Image src="/logo_white.png" alt="Logo Code124" fill priority />
+            </div>
+            <div className="text-base font-semibold lg:text-lg">CODE124</div>
           </div>
           <div>
             <h3 className="mb-2 font-bold">#TheFirstCommit</h3>
-            <p className="max-w-[350px] text-[14px] leading-relaxed text-gray-300">
+            <p className="max-w-[300px] text-left text-[13px] leading-relaxed text-gray-300 lg:max-w-[350px] lg:text-[14px]">
               In Engineering We Trust, In Innovation We Rise. Designing Tomorrow
               Today. Powered By Knowledge, Driven By Innovation. Because Every
               Problem Has A Solution.
             </p>
           </div>
         </div>
-        <div className="flex flex-col space-y-4 md:w-1/2 md:pl-5">
+
+        {/* Kolom Kanan: Social Media */}
+        <div className="flex w-full flex-col items-start space-y-3 pt-8 lg:w-1/2 lg:space-y-4 lg:pt-0 lg:pl-5">
           {socialMediaData.map((social) => (
             <SocialMediaLink
               key={social.platform}
@@ -56,10 +62,8 @@ export function Footer() {
         </div>
       </div>
 
-      {/* garis vertikal di tengah */}
-      <div className="absolute top-1/2 left-1/2 hidden h-[220px] -translate-x-1/2 -translate-y-1/2 border-r border-white md:block" />
+      {/* Garis Vertikal di Desktop */}
+      <div className="absolute top-1/2 left-1/2 hidden h-[220px] -translate-x-1/2 -translate-y-1/2 border-r border-white lg:block" />
     </footer>
   );
 }
-
-export default Footer;
