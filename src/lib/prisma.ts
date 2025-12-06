c522e36b4607e4d2fb5f071f7ca0e86fe2cf9e1d
andhikaguntur <andhikaguntur77.ag@gmail.com>
2025-12-07 01:11:53 +0700

    feat:add prisma

---
+++ src/lib/prisma.ts
@@ -0,0 +1,6 @@
+import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const prisma = new PrismaClient({
+  accelerateUrl: process.env.DATABASE_URL,
+}).$extends(withAccelerate())
