dbf701e86e2b6df39d03102b58b07efe9716687d
IvandryPage <ivandry.89@gmail.com>
2025-12-07 11:00:17 +0700

    fix: mobile navigation

--- src/app/components/sections/Navigation/Navigation.mobile.tsx
+++ src/app/components/sections/Navigation/Navigation.mobile.tsx
@@ -7,35 +7,41 @@ export function Navigation({ className }: { className: string }) {
 
   return (
     <div className={`${className} relative`}>
+      {/* BUTTON */}
       <button
-        className="relative z-20 flex flex-col space-y-1.5 p-2"
+        className="bg-primary-500 relative z-25 flex flex-col items-center justify-center space-y-1.5 p-2"
         onClick={() => setOpen(!open)}
         aria-label="Toggle menu"
       >
+        {/* Top bar */}
         <span
-          className={`block h-1 w-6 rounded-2xl bg-black transition-transform ${
-            open ? 'rotate-60' : ''
+          className={`block h-1 w-6 rounded-2xl bg-black transition-transform duration-300 ${
+            open ? 'translate-y-3 rotate-45' : ''
           }`}
         />
+
+        {/* Middle bar */}
         <span
-          className={`block h-1 w-6 rounded-2xl bg-black transition-opacity ${
+          className={`block h-1 w-6 rounded-2xl bg-black transition-opacity duration-300 ${
             open ? 'opacity-0' : 'opacity-100'
           }`}
         />
+
+        {/* Bottom bar */}
         <span
-          className={`block h-1 w-6 rounded-2xl bg-black transition-transform ${
-            open ? '-rotate-60' : ''
+          className={`block h-1 w-6 rounded-2xl bg-black transition-transform duration-300 ${
+            open ? '-translate-y-2 -rotate-45' : ''
           }`}
         />
       </button>
 
+      {/* MENU */}
       <div
-        className={`absolute top-20 right-0 flex flex-col gap-4 space-y-2 rounded-lg transition-all duration-300 ${
-          open
-            ? 'pointer-events-auto translate-y-0 opacity-100'
-            : 'pointer-events-none -translate-y-4 opacity-0'
-        }`}
+        className={`bg-secondary-300 absolute -top-5 -right-6 flex w-[50vw] flex-col gap-4 p-5 pt-20 transition-all duration-300 ${open ? 'pointer-events-auto translate-y-0 opacity-100' : 'pointer-events-none -translate-y-4 opacity-0'} `}
       >
+        {/* Separator line */}
+        <div className="bg-secondary-400 mb-2 h-[2px] w-full opacity-40" />
+
         <NavLink href="/">HOME</NavLink>
         <NavLink href="/the-team">THE TEAM</NavLink>
         <NavLink href="/our-past">OUR PAST</NavLink>
